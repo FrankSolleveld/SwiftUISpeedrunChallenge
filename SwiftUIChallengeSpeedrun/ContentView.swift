60da5caa27080482cf47698dd55404fc8af0a2e6
Frank <fsolleveld@outlook.com>
2022-05-19 09:08:48 +0200

    Initial Commit

---
+++ SwiftUIChallengeSpeedrun/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIChallengeSpeedrun
+//
+//  Created by Frank Solleveld on 19/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIChallengeSpeedrun/SwiftUIChallengeSpeedrunApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChallengeSpeedrunApp.swift
+//  SwiftUIChallengeSpeedrun
+//
+//  Created by Frank Solleveld on 19/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChallengeSpeedrunApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
